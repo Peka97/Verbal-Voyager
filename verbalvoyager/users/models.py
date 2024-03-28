3ac2b853091dbadcfdcf35c5830bd3906c20df15
peka97 <ivan.karasyov97@gmail.com>
2024-03-28 13:52:19 +0400

    Start reworked app. Create custom User model

---
+++ verbalvoyager/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    # def is_teacher(self):
+    #     try:
+    #         return True if self.groups.filter(name='Teacher').exists() else False
+    #     except ValueError:
+    #         return False
+    pass
