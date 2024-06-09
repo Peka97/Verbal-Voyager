b22c334e76e94ebf7960411c423c220bf962e114
peka97 <ivan.karasyov97@gmail.com>
2024-06-09 19:34:10 +0000

    Minor fix

---
+++ verbalvoyager/load_fixtures_data.sh
@@ -0,0 +1,9 @@
+python3 manage.py loaddata /home/peka97/Verbal-Voyager/verbalvoyager/users/fixtures/subjects.json
+echo "Users loaded successfully"
+python3 manage.py loaddata /home/peka97/Verbal-Voyager/verbalvoyager/event_calendar/fixtures/subjects.json
+echo "Event Calendar loaded successfully"
+python3 manage.py loaddata /home/peka97/Verbal-Voyager/verbalvoyager/exercises/fixtures/subjects.json
+echo "Exercises loaded successfully"
+python3 manage.py loaddata /home/peka97/Verbal-Voyager/verbalvoyager/pages/fixtures/subjects.json
+echo "Pages loaded successfully"
+
