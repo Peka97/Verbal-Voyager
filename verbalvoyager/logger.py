66e57852c7cd47cfcceff023b1793c3e8225c15e
peka97 <ivan.karasyov97@gmail.com>
2025-04-24 19:33:41 +0300

    Minor fix

--- verbalvoyager/logger.py
+++ verbalvoyager/logger.py
@@ -3,20 +3,19 @@ import logging
 from django.conf import settings
 
 
-
 def get_logger(level=logging.ERROR):
     logger = logging.getLogger()
     logger.setLevel(level)
     log_format = "%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
 
-    if level == logging.DEBUG:
-        handler = logging.FileHandler(settings.CURRENT_CONFIG.DEBUG_LOG_FILE_PATH)
+    if settings.LOGGING['handlers'].get('file'):
+        if level == logging.DEBUG:
+            handler = logging.FileHandler(settings.CURRENT_CONFIG.DEBUG_LOG_FILE_PATH)
+        else:
+            handler = logging.FileHandler(settings.CURRENT_CONFIG.DJANGO_LOG_FILE_PATH)
 
-    else:
-        handler = logging.FileHandler(settings.CURRENT_CONFIG.DJANGO_LOG_FILE_PATH)
-
-    handler.setFormatter(logging.Formatter(log_format))
-    logger.addHandler(handler)
+        handler.setFormatter(logging.Formatter(log_format))
+        logger.addHandler(handler)
 
     return logger
 
@@ -26,9 +25,10 @@ def get_words_logger(level=logging.ERROR):
     logger.setLevel(level)
     log_format = "%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
 
-    handler = logging.FileHandler(settings.CURRENT_CONFIG.WORDS_LOG_FILE_PATH)
+    if settings.LOGGING['handlers'].get('file'):
+        handler = logging.FileHandler(settings.CURRENT_CONFIG.WORDS_LOG_FILE_PATH)
 
-    handler.setFormatter(logging.Formatter(log_format))
-    logger.addHandler(handler)
+        handler.setFormatter(logging.Formatter(log_format))
+        logger.addHandler(handler)
 
     return logger
