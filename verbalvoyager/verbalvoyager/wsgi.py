49f8a665fde125ba7f395ad7498f2e57729d6ad4
peka97 <ivan.karasyov97@gmail.com>
2025-04-24 18:30:33 +0300

    Minor fix

--- verbalvoyager/verbalvoyager/wsgi.py
+++ verbalvoyager/verbalvoyager/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-if not os.getenv('DJANGO_CI_SETTINGS_MODULE'):
+
+if not os.getenv('DJANGO_SETTINGS_MODULE'):
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'verbalvoyager.settings')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'verbalvoyager.settings_ci')
 
 application = get_wsgi_application()
