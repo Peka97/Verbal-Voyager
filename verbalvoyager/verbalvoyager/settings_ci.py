d1125c2003f713aee53884ed8814eb8bbee94653
peka97 <ivan.karasyov97@gmail.com>
2025-04-24 23:15:08 +0300

    Minor fix

--- verbalvoyager/verbalvoyager/settings_ci.py
+++ verbalvoyager/verbalvoyager/settings_ci.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'verbalvoyager.settings_ci')
 
 DEBUG = False
 
